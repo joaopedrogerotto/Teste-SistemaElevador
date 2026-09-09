c0c1ece173ecec9ec6bb1454cb860a7ddef8bed8
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-09-08 21:00:50 -0300

    feat: elevador factory

---
+++ SistemaElevador/Factory/ElevadorFactory.cs
@@ -0,0 +1,25 @@
+﻿using SistemaElevador.Enums;
+using SistemaElevador.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaElevador.Factory {
+    internal class ElevadorFactory {
+        public ElevadorFactory() {
+            
+        }
+
+        public Elevador Configurar() {
+            return new Elevador {
+                AndarAtual = 0,
+                AndarMaximo = 10,
+                MaximoPassageiros = 5,
+                QuantidadePassageiros = 0,
+                Status = StatusElevadorEnum.Parado,
+                StatusPorta = StatusPortaEnum.Aberta,
+                Rota = new List<int>()
+            };
+        }
+    }
+}
